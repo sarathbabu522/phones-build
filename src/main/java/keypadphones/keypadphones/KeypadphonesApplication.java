5ccd3c7356fd3b0146142045c14afb9462a38a95
root <root@ip-172-31-63-239.ec2.internal>
2023-09-05 10:38:24 +0000

    new phones build

---
+++ src/main/java/keypadphones/keypadphones/KeypadphonesApplication.java
@@ -0,0 +1,13 @@
+package keypadphones.keypadphones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeypadphonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeypadphonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keypadphones/keypadphones/KeypadphonesApplicationTests.java
@@ -0,0 +1,13 @@
+package keypadphones.keypadphones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeypadphonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
